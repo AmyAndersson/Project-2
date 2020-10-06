60d910669feae1c8b8692333dd1aa6dba2f02152
BAS <63766016+BonnerDev@users.noreply.github.com>
2020-10-06 22:04:37 +0800

    commit flask app

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
 from flask import Flask, render_template
 from flask import Flask, jsonify
+from sqlalchemy import create_engine
+import pandas as pd
+from config import username
+from config import password
+from config import database
 
 app = Flask(__name__)
 
-#create conection
-# engine = create_engine("sqlite:///")
+#create connection to databse
+engine = create_engine(f"postgresql://{username}:{password}@localhost:5432/{database}")
+conn=engine.connect()
 
-# Connect to a database. 
-db1 = client.data
 
-@app.route("/file1")
+
+# what do we want this to do when the index route
+@app.route("/dataone")
 def index():
-    species = mongo.datagovau.find_one()
-    return render_template("index.html", listings=species)
+    data1 = pd.read_sql("SELECT * FROM dataone LIMIT 10",conn)
+    var = data1.to_json(orient="records")
+    return jsonify(var)
 #^this will need to return json file 
 
 if __name__ == "__main__":
