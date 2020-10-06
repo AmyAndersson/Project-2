af15e0b763fb3eafc8ebad03521587c6b8d19abf
BAS <63766016+BonnerDev@users.noreply.github.com>
2020-10-06 16:29:19 +0800

    change to chema

--- databaseschema.sql
+++ databaseschema.sql
@@ -34,5 +34,34 @@ create table spcs (
 
 );
 
-
+create table species (
+	
+	index int,
+	species varchar,
+	common_name_3 varchar,
+	category varchar,
+	presence_type varchar,
+	kingodm_3 varchar,
+	Phylum_3 varchar,
+	Class_3 varchar,
+	Order_3 varchar,
+	Family_3 varchar,
+	Location_1 varchar,
+	Location_2 varchar,
+	Location_3 varchar,
+	Location_4 varchar,
+	Location_5 varchar,
+	Location_6 varchar,
+	Location_7 varchar,
+	Location_8 varchar,
+	Location_9 varchar,
+	Location_10 varchar,
+	Location_11 varchar,
+	Location_12 varchar,
+	Location_13 varchar,
+	Location_14 varchar,
+	Location_15 varchar,
+	Location_16 varchar
+	
+);
 
